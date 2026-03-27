ed3f219e68224781c91f355b1350e99935410283
Hugh Han <hughhan1@users.noreply.github.com>
2026-03-26 20:31:25 -0400

    chore: remove provider inventory fixture (#225)

--- plugins/providers/jira/factory_test.go
+++ plugins/providers/jira/factory_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/valon-technologies/gestalt/core"
 	"github.com/valon-technologies/gestalt/internal/config"
-	"github.com/valon-technologies/gestalt/plugins/providers/inventory"
 	"github.com/valon-technologies/gestalt/plugins/providers/providertest"
 )
 
@@ -13,23 +12,29 @@ const (
 	dummyClientID     = "dummy-client-id"
 	dummyClientSecret = "dummy-client-secret"
 	discoveryURL      = "https://api.atlassian.com/oauth/token/accessible-resources"
+	expectedAuthType  = "oauth2"
+	expectedConnMode  = "user"
 )
+
+var expectedOperationNames = []string{
+	"add_comment",
+	"create_issue",
+	"get_issue",
+	"get_transitions",
+	"list_projects",
+	"search_issues",
+	"transition_issue",
+}
 
 func TestDefinitionParses(t *testing.T) {
 	t.Parallel()
 
-	inv, err := inventory.Load()
-	if err != nil {
-		t.Fatalf("inventory.Load: %v", err)
-	}
-	spec := inv.Providers["jira"]
-
 	def := providertest.ParseDefinition(t, definitionYAML)
 	providertest.CheckDefinition(t, def, providertest.DefinitionExpect{
 		Name:           "jira",
-		OperationCount: len(spec.Operations),
-		AuthType:       spec.AuthType,
-		ConnectionMode: spec.ConnectionMode,
+		OperationCount: len(expectedOperationNames),
+		AuthType:       expectedAuthType,
+		ConnectionMode: expectedConnMode,
 		Connection: map[string]providertest.ConnParam{
 			"cloud_id": {Required: true, From: "discovery"},
 		},
@@ -39,12 +44,6 @@ func TestDefinitionParses(t *testing.T) {
 func TestBuildProvider(t *testing.T) {
 	t.Parallel()
 
-	inv, err := inventory.Load()
-	if err != nil {
-		t.Fatalf("inventory.Load: %v", err)
-	}
-	spec := inv.Providers["jira"]
-
 	def := providertest.ParseDefinition(t, definitionYAML)
 	prov := providertest.BuildProvider(t, def, config.IntegrationDef{
 		ClientID:     dummyClientID,
@@ -53,9 +52,9 @@ func TestBuildProvider(t *testing.T) {
 
 	providertest.CheckProvider(t, prov, providertest.ProviderExpect{
 		Name:           "jira",
-		ConnectionMode: core.ConnectionMode(spec.ConnectionMode),
-		OperationCount: len(spec.Operations),
-		OperationNames: spec.Operations,
+		ConnectionMode: core.ConnectionMode(expectedConnMode),
+		OperationCount: len(expectedOperationNames),
+		OperationNames: expectedOperationNames,
 	})
 
 	providertest.CheckDiscovery(t, prov, providertest.DiscoveryExpect{
