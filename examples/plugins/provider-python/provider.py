e5e1f23dd6d45ca35c040b4a65fd7e66889fcea7
Hugh Han <hughhan1@gmail.com>
2026-04-06 21:15:54 -0400

    Simplify Python SDK example

--- examples/plugins/provider-python/provider.py
+++ examples/plugins/provider-python/provider.py
@@ -1,7 +1,5 @@
 import gestalt
 
-GREETING = "Hello"
-
 
 class GreetInput(gestalt.Model):
     name: str = gestalt.field(description="Name to greet", default="World")
@@ -11,15 +9,10 @@ class GreetOutput(gestalt.Model):
     message: str
 
 
-def configure(_name: str, config: dict[str, object]) -> None:
-    global GREETING
-    GREETING = str(config.get("greeting", "Hello"))
-
-
 @gestalt.operation(
     method="GET",
     description="Return a greeting message",
     read_only=True,
 )
 def greet(input: GreetInput, _req: gestalt.Request) -> GreetOutput:
-    return GreetOutput(message=f"{GREETING}, {input.name}!")
+    return GreetOutput(message=f"Hello, {input.name}!")
