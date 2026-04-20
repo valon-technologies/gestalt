566c1c2adfe34284a520d74f637a0b2cdd047c72
Hugh Han <hughhan1@users.noreply.github.com>
2026-04-20 12:28:32 -0400

    docs: add workflow and authorization provider docs (#1155)

--- docs/content/custom-providers/_meta.ts
+++ docs/content/custom-providers/_meta.ts
@@ -1,10 +1,12 @@
 export default {
   plugins: "Plugins",
   auth: "Auth",
+  authorization: "Authorization",
   cache: "Cache",
   indexeddb: "IndexedDB",
   s3: "S3",
   secrets: "Secrets",
+  workflow: "Workflow",
   "web-uis": "Web UIs",
   releasing: "Releasing",
 };
