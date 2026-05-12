81921b55ad8a31fe8b9609a6ba1d7193a9ce940e
Hugh Han <hughhan1@users.noreply.github.com>
2026-05-12 10:15:46 -0400

    Default to stable UI provider release (#1933)

--- gestaltd/services/runtimehost/runtime_log_host_server_test.go
+++ gestaltd/services/runtimehost/runtime_log_host_server_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/valon-technologies/gestalt/server/services/runtimehost/runtimelogs"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/connectivity"
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -25,17 +26,22 @@ func newRuntimeLogHostTestClient(t *testing.T, socket string) proto.PluginRuntim
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	conn, err := grpc.DialContext(ctx, "passthrough:///localhost",
+	conn, err := grpc.NewClient("passthrough:///localhost",
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
 			var d net.Dialer
 			return d.DialContext(ctx, "unix", socket)
 		}),
 		grpc.WithAuthority("localhost"),
-		grpc.WithBlock(),
 	)
 	if err != nil {
-		t.Fatalf("dial runtime log host: %v", err)
+		t.Fatalf("create runtime log host client: %v", err)
+	}
+	conn.Connect()
+	for state := conn.GetState(); state != connectivity.Ready; state = conn.GetState() {
+		if !conn.WaitForStateChange(ctx, state) {
+			t.Fatalf("dial runtime log host: %v", ctx.Err())
+		}
 	}
 	t.Cleanup(func() { _ = conn.Close() })
 	return proto.NewPluginRuntimeLogHostClient(conn)
