879a7d26d4e4a3bcdb4aef28a43c06c3018636be
Hugh Han <hughhan1@gmail.com>
2026-05-22 17:47:15 -0400

    Remove legacy workflow host env constant

--- gestaltd/services/workflows/env.go
+++ gestaltd/services/workflows/env.go
@@ -1,13 +1,6 @@
 package workflows
 
-const (
-	DefaultHostSocketEnv     = "GESTALT_WORKFLOW_HOST_SOCKET"
-	DefaultProviderSocketEnv = "GESTALT_WORKFLOW_PROVIDER_SOCKET"
-)
-
-func HostSocketTokenEnv() string {
-	return DefaultHostSocketEnv + "_TOKEN"
-}
+const DefaultProviderSocketEnv = "GESTALT_WORKFLOW_PROVIDER_SOCKET"
 
 func ProviderSocketTokenEnv() string {
 	return DefaultProviderSocketEnv + "_TOKEN"
