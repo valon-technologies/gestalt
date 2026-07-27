0278fa87a12e0638ed12cf18ee2faaa3ab2b89b7
Hugh Han <hughhan1@gmail.com>
2026-07-27 10:34:00 -0400

    Bump gestaltd formula and chart to v0.0.2-alpha.47

--- Formula/gestaltd.rb
+++ Formula/gestaltd.rb
@@ -3,30 +3,30 @@
 class Gestaltd < Formula
   desc "Gestalt server daemon"
   homepage "https://github.com/valon-technologies/gestalt"
-  version "0.0.2-alpha.46"
+  version "0.0.2-alpha.47"
   license "Apache-2.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.46/gestaltd-macos-arm64.tar.gz"
-      sha256 "76b8280a83d4bbdc5e4e595b2e0872d3d7172b3d802ba704857baa5425c4f59e"
+      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.47/gestaltd-macos-arm64.tar.gz"
+      sha256 ""
     end
 
     on_intel do
-      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.46/gestaltd-macos-x86_64.tar.gz"
-      sha256 "97c0971edb97da43eafa650d681d0c612342474a4ff11003655d00acf10e7303"
+      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.47/gestaltd-macos-x86_64.tar.gz"
+      sha256 ""
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.46/gestaltd-linux-arm64.tar.gz"
-      sha256 "f52107fca1ed55ceff3e8225704172437128b216fc284788f62f12aed2399285"
+      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.47/gestaltd-linux-arm64.tar.gz"
+      sha256 ""
     end
 
     on_intel do
-      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.46/gestaltd-linux-x86_64.tar.gz"
-      sha256 "4a76c93c2f62bf85795bcf76fc65599b7121355d2437f6f6f4babe26677a837e"
+      url "https://github.com/valon-technologies/gestalt/releases/download/gestaltd/v0.0.2-alpha.47/gestaltd-linux-x86_64.tar.gz"
+      sha256 ""
     end
   end
 
